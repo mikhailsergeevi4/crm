81127eea2c105545a9359014cadc8b46d966122e
mikhailsergeevi4 <mikhailsergeevi4@yandex.ru>
2018-04-18 17:23:15 +0500

    remove registration system

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -33,19 +33,19 @@ def logout():
     return redirect(url_for('main.index'))
 
 
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('auth.login'))
-    return render_template('auth/register.html', title='Register', form=form)
+#@bp.route('/register', methods=['GET', 'POST'])
+#def register():
+    #if current_user.is_authenticated:
+        #return redirect(url_for('main.index'))
+    #form = RegistrationForm()
+    #if form.validate_on_submit():
+        #user = User(username=form.username.data, email=form.email.data)
+        #user.set_password(form.password.data)
+        #db.session.add(user)
+        #db.session.commit()
+        #flash('Congratulations, you are now a registered user!')
+        #return redirect(url_for('auth.login'))
+    #return render_template('auth/register.html', title='Register', form=form)
 
 
 @bp.route('/reset_password_request', methods=['GET', 'POST'])
