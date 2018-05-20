fca2b5841fb729a7ab7cc0e72002fd00be851c59
mikhailsergeevi4 <mikhailsergeevi4@yandex.ru>
2018-05-20 21:17:44 +0500

    remove personal data

--- insert/insert.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sqlalchemy import *
-
-engine = create_engine('mysql+pymysql://crm:1umtc1@localhost:3306/crm')
-df = pd.read_csv('/home/ubuntu/crm/insert/id2.csv', delimiter=';')
-df.to_sql('clinic', con=engine, flavor='mysql') # replace truncates the existing table and creates a new one
